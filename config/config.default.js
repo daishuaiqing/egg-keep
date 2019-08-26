599ded4e212d858fbefbbdeb2240a699b777119d
daishuaiqing <daishuaiqing@163.com>
2019-08-26 15:10:25 +0800

    修改相关配置，测试接口通过

--- config/config.default.js
+++ config/config.default.js
@@ -10,7 +10,16 @@ module.exports = appInfo => {
    * built-in config
    * @type {Egg.EggAppConfig}
    **/
-  const config = exports = {};
+  const config = exports = {
+    sequelize : {
+      dialect: 'mysql',
+      host: '127.0.0.1',
+      port: 3306,
+      database: 'keepline',
+      username: 'root',
+      password: '123456',
+    }
+  };
 
   // use for cookie sign key, should change to your own and keep security
   config.keys = appInfo.name + '_1566266521656_7212';
